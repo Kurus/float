3c6fc3501341c345d197f479edb903f3b4a7f4bf
Pradeep <deep0kathir@gmail.com>
2018-05-29 19:53:06 +0530

    float12 mult

--- float_12_mul.c
+++ float_12_mul.c
@@ -15,15 +15,23 @@ union float32 {
 float b2f(char a){
 	union float32 aa;
 	aa.r.sgn=(a&0x80)>>7;
-	aa.r.exp=(((a&0x7c)>>2)+112);
 	aa.r.man=(a&0x3)<<21;
+	if(((a&0x7c)>>2) == 0){
+		aa.r.exp=0;
+	}
+	else{
+		aa.r.exp=(((a&0x7c)>>2)+112);
+	}
 	return aa.f;
 }
 
 char f2b(float a){
 	union float32 aa;
 	aa.f=a;
-	return aa.r.sgn<<7 | (aa.r.exp-112)<<2 | aa.r.man>>21;
+	char x = (aa.r.exp-112);
+	if(x<0)x=0;
+	if(x>31)x=31;
+	return aa.r.sgn<<7 | x<<2 | aa.r.man>>21;
 }
 
 void mult(char a, char b, char* c,char* a_out, char* b_out, char en){
@@ -49,11 +57,13 @@ void mult(char a, char b, char* c,char* a_out, char* b_out, char en){
 int main()
 {
 	char ans,aa,bb;
-	for (int i = 0; i < 10; ++i)
-	{
-		mult(61,60+i,&ans,&aa,&bb,i%2);
-		printf("%d %d %d \n", ans,aa,bb);
-	}
+		mult(0,0,&ans,&aa,&bb,1);
+	printf("%d %d %d \n", ans,aa,bb);
+	// for (int i = 0; i < 10; ++i)
+	// {
+	// 	mult(0,0,&ans,&aa,&bb,i%2);
+	// 	printf("%d %d %d \n", ans,aa,bb);
+	// }
 	
 	return 0;
 }
